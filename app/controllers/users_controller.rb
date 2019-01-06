b1537403164156a40cc9d7ccfd994390d66be550
Oishi Yuikyo <s2008511@gmail.com>
2019-01-07 01:32:56 +0900

    実験したよ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,34 @@
 class UsersController < ApplicationController
 
-
   def show
     @user = User.find(params[:id])
     @blogs = Blog.where(user_id:params[:id]).order('created_at desc').page(params[:page]).per(3)
   end
 
   def index
+    @users = User.all
+
+    @each_ids = []
+    @users.each do |user|
+      @each_ids << user.id
+    end
+
+    @a7times_ids = []
+    @users.map do |user|
+      @a7times_ids << user.id * 7
+    end
+
+    @map¥names = []
+    @users.map do |user|
+      @map¥names << "#{user.name}ちゃんで〜す"
+    end
+
   end
-
-
 end
+
+# コントローラーで繰り返し処理できる！
+# rubyのメソッドとか使えそう。map, each とか。他のも試したい。
+# 変数は記号がほとんど使えなかったり、冒頭文字に数字だめなど制約あり。
+#
+#
+#
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "home#top"
+  devise_scope :user do
+    get '/users/sign_in2' => 'devise/sessions#new', as: :new_user_session2
+  end
 
   resources :blogs do
     resources :comments,only:[:create]
   end
   resources :users,only:[:show, :index]
-
   get "home/vue" => "home#vue"
+  root "home#top"
 end
 
